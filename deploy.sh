46b7090f8aa087c8a9b4874579c1d2802254b135
stillirrom <irrom@hotmail.com>
2022-02-05 14:33:10 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:digitaltolkien/vue-wordle.git main:gh-pages
+git push -f git@github.com:digitaltolkien/vue-wordle.git master:gh-pages
 
 cd -
